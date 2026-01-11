e2fad4ae8d367efe24c39c7032828be1397aea40
unknown <veraestelle463gmail.com>
2026-01-11 22:23:57 +0100

    adding business rule for adding a visit

--- CleanVisitor.Application/UseCases/Visits/Commands/AddVisit/AddVisitHandler.cs
+++ CleanVisitor.Application/UseCases/Visits/Commands/AddVisit/AddVisitHandler.cs
@@ -6,12 +6,22 @@ namespace CleanVisitor.Application.UseCases.Visits.Commands.AddVisit;
 public class AddVisitHandler
 {
     private readonly IVisitRepository _repository;
-    public AddVisitHandler(IVisitRepository repository)
+    private readonly IVisitorRepository _visitorRepository;
+    public AddVisitHandler(IVisitRepository repository,
+                            IVisitorRepository visitorRepository)
     {
         _repository = repository;
+        _visitorRepository = visitorRepository;
     }
     public async Task Handle(AddVisitCommand command)
     {
+        //Verify if the visitor exist
+        var visitor = await _visitorRepository.GetByIdAsync(command.IdVisitor);
+        if(visitor == null)
+            throw new Exception("Visitor not found");
+
+        //verify the visitor hasn't any visit on
+        //var activeVisit = await _visitorRepository.G
         var visit = new Visit
         {
             IdVisitor = command.IdVisitor,
